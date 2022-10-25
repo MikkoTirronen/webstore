319db67e4937a046125cf4e560e0b5be81586226
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-25 22:40:57 +0200

    added dropdown to select number of items shown

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -3,15 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
-  styleUrls: ['./products-header.component.css']
+  styleUrls: ['./products-header.component.css'],
 })
 export class ProductsHeaderComponent implements OnInit {
   sort = 'Descending';
-  constructor() { }
+  itemsShowCount = 12;
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  onSortUpdated(newSort: string): void{
+  ngOnInit(): void {}
+  onSortUpdated(newSort: string): void {
     this.sort = newSort;
+  }
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
   }
 }
