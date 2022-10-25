779e8dd2a61cc737b89c8ec09c1b2282a5d9076a
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-25 23:11:12 +0200

    added Categories Dropdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { MatListModule } from '@angular/material/list';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
 
 
 
@@ -29,7 +30,8 @@ import { ProductsHeaderComponent } from './pages/home/components/products-header
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ProductsHeaderComponent
+    ProductsHeaderComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
