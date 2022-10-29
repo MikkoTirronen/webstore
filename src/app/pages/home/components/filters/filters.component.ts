662d28adc119e721dbc96ec7409c5687eb144968
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-29 23:13:40 +0200

    added clear category button

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -15,16 +15,22 @@ import { StoreService } from 'src/app/services/store.service';
   template: `
     <mat-expansion-panel *ngIf="categories">
       <mat-expansion-panel-header>
-        <mat-panel-title
-          >CATEGORIES</mat-panel-title
-        ></mat-expansion-panel-header
-      >
+        <mat-panel-title>CATEGORIES</mat-panel-title>
+      </mat-expansion-panel-header>
       <mat-selection-list [multiple]="false">
         <mat-list-option *ngFor="let category of categories" [value]="category">
           <button (click)="onShowCategory(category)">{{ category }}</button>
         </mat-list-option>
       </mat-selection-list>
     </mat-expansion-panel>
+    <div  class="flex justify-center pt-3">
+      <button
+        (click)="onShowCategory('')"
+        type="button"
+        class="mb-2 w-full inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+      >
+        Reset Categories</button
+      >
   `,
 })
 export class FiltersComponent implements OnInit, OnDestroy {
