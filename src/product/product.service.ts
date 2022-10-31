14362deb8c3bc5c56f56f9cb70f25b6ea6cb430f
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-31 15:00:53 +0100

    added constructor

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { Product } from './product.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from './product.schema';
 
 @Injectable()
 export class ProductService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
   private readonly products: Product[] = [
     {
       id: 1,
