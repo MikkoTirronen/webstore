11cbeec95f19d78b621ebd61dcfe4dfdef129f67
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-31 14:10:18 +0100

    added findOne

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -11,6 +11,14 @@ export class ProductController {
     return this.productService.findAll();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: number): Product | undefined {
+    this.logger.debug(`Searching for Todo with id: ${id}`);
+    const product = this.productService.findOne(id);
+    this.logger.debug(`Found product: ${JSON.stringify(product)}`);
+    return product;
+  }
+
   @Post()
   create(@Body() product: Product) {
     this.logger.debug(`New Product ${JSON.stringify(product)}`);
