a8c285a09c31feacc52c85db0d0e54c4cb6c4c5a
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-10-31 14:59:38 +0100

    added mongoose and types

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/myproducts')],
   controllers: [ProductController],
   providers: [ProductService],
 })
