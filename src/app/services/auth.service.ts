fe7931c6d6d32922937616c6e249605c4d1a8618
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-06 21:32:34 +0100

    added login service http call

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(email: string, password: string) {
+    return this.http
+      .post<User>('api/login', { email, password })
+      .pipe(shareReplay());
+    //Rest of the Token Logic
+  }
 }
