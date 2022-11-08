231e00422c1552c27c4ab04ba195f54b4de66100
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-08 22:28:54 +0100

    added controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,7 +3,6 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './users.model';
-import { UsersController } from './users.controller';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'user', schema: UserSchema }])],
