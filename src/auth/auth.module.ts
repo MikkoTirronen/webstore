6d8c4ccc9992309f4fd3b69213ebdc8b8ea56cc8
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-08 22:03:59 +0100

    added auth.module imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UsersService } from 'src/user/user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { UserModule } from 'src/users/users.module';
+import { UserSchema } from 'src/users/users.model';
+import { LocalStrategy } from './strategies/local.auth';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '60s' },
+    }),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
+  providers: [AuthService, UsersService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
