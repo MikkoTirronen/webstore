db055bbe98b2d47d71f26f710b8fb70264d55f6b
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-08 17:47:15 +0100

    added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,23 @@ import { ProductController } from './product/product.controller';
 import { Product, ProductSchema } from './product/product.schema';
 import { ProductService } from './product/product.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user/user/user.schema';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/myproducts'),
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   controllers: [ProductController],
   providers: [ProductService],
