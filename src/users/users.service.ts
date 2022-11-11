06c6129c13b8d20415f4f4a09677e5037733c4bc
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-11 18:06:31 +0100

    added more parameters to createUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,15 +3,34 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './users.model';
 
+export interface CartItem {
+  product: string;
+  name: string;
+  price: number;
+  quantity: number;
+  id: number;
+}
+
 @Injectable()
 export class UsersService {
   constructor(
     @InjectModel('user') private readonly userModel: Model<UserDocument>,
   ) {}
-  async createUser(username: string, password: string): Promise<User> {
+  async createUser(
+    username: string,
+    password: string,
+    name: string,
+    telephone: number,
+    address: string,
+    tempCart?: Array<CartItem>,
+  ): Promise<User> {
     return this.userModel.create({
       username,
       password,
+      name,
+      telephone,
+      address,
+      tempCart,
     });
   }
   async getUser(query: object): Promise<User | null> {
