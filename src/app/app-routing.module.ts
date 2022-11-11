b41c559f13693b5526d585f1c27b25acb9631cc2
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-11 23:16:12 +0100

    added routes for new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './pages/cart/cart.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'signup', component: SignUpComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
 ];
 
