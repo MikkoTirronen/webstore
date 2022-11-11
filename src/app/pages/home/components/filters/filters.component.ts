05e3da141a5e0883903ffbddb52e401323781a37
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-11 17:52:58 +0100

    added button for testing

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -23,14 +23,16 @@ import { StoreService } from 'src/app/services/store.service';
         </mat-list-option>
       </mat-selection-list>
     </mat-expansion-panel>
-    <div  class="flex justify-center pt-3">
+    <div class="flex justify-center pt-3">
       <button
         (click)="onShowCategory('')"
         type="button"
         class="mb-2 w-full inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
       >
-        Reset Categories</button
-      >
+        Reset Categories
+      </button>
+  
+    </div>
   `,
 })
 export class FiltersComponent implements OnInit, OnDestroy {
