0c193d6a0a843e2b90092ab24df071a01c4fc6c9
Mikko Tirronen <mikkoatirronen@gmail.com>
2022-11-11 17:54:00 +0100

    added jwt login logic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,55 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
-import { shareReplay } from 'rxjs/operators';
+import { shareReplay, tap } from 'rxjs/operators';
+import * as moment from 'moment';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) {}
 
-  login(email: string, password: string) {
-    return this.http
-      .post<User>('api/login', { email, password })
-      .pipe(shareReplay());
+  login(username: string, password: string) {
+    return (
+      this.http
+        .post<User>('api/auth/login', { username, password })
+        .pipe(
+          tap((data) => this.setSession(data)),
+          shareReplay()
+        )
+    );
     //Rest of the Token Logic
   }
+
+  private setSession(authResult: any) {
+    const expiresAt = moment().add(authResult.expiresIn, 'second');
+    console.log('Setting token to Local Storage: ', authResult.access_token);
+    localStorage.setItem('id_token', authResult.access_token);
+    localStorage.setItem('expires_at', JSON.stringify(expiresAt.valueOf()));
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration!);
+    return moment(expiresAt);
+  }
+
+
+
+
 }
